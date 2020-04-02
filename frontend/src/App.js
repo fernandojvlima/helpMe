bff17a80f01186159beb47a2f0b9b8182ff4ec78
fernandojvlima <fernandojvlima@gmail.com>
2020-04-02 20:44:45 -0300

    Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import Header from './components/Header';
+
+function App() {
+
+
+  return (
+    <div>
+      <Header>Semana Omnistack </Header>
+
+    </div>
+  )
+}
+export default App;
